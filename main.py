bfd17c45699d9545e5333996e1d42fa8ba7b7efd
user <william.schuch@wur.nl>
2016-01-18 16:01:59 +0100

    AddedTheCoordinates

--- main.py
+++ main.py
@@ -23,8 +23,86 @@ if drv is None:
 else:
     print  "%s driver IS available.\n" % driverName
 
+## choose your own name
+## make sure this layer does not exist in your 'data' folder
+fn = "Two_points_GEOU.shp"
+layername = "2POINTS"
+
+## Create shape file
+ds = drv.CreateDataSource(fn)
+print ds.GetRefCount()
+
+# Set spatial reference
+spatialReference = osr.SpatialReference()
+spatialReference.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+
+# you can also do the following
+# spatialReference.ImportFromEPSG(4326)
+
+
+## Create Layer
+layer=ds.CreateLayer(layername, spatialReference, ogr.wkbPoint)
+## Now check your data folder and you will see that the file has been created!
+## From now on it is not possible anymore to CreateDataSource with the same name
+## in your workdirectory untill your remove the name.shp name.shx and name.dbf file.
+print(layer.GetExtent())
+
+## What is the geometry type???
+## What does wkb mean??
+
+## ok lets leave the pyramid top and start building the bottom,
+## let's do points
+## Create a point
+point1 = ogr.Geometry(ogr.wkbPoint)
+point2 = ogr.Geometry(ogr.wkbPoint)
+
+## SetPoint(self, int point, double x, double y, double z = 0)
+point1.SetPoint(0,12.08162866,-68.875088) 
+point2.SetPoint(0,52.307267,4.768967)
+
+## Actually we can do lots of things with points: 
+## Export to other formats/representations:
+print "KML file export"
+print point2.ExportToKML()
+
+## Buffering
+buffer = point2.Buffer(4,4)
+print buffer.Intersects(point1)
+
+## More exports:
+buffer.ExportToGML()
+
+## Back to the pyramid, we still have no Feature
+## Feature is defined from properties of the layer:e.g:
+
+layerDefinition = layer.GetLayerDefn()
+feature1 = ogr.Feature(layerDefinition)
+feature2 = ogr.Feature(layerDefinition)
+
+## Lets add the points to the feature
+feature1.SetGeometry(point1)
+feature2.SetGeometry(point2)
+
+## Lets store the feature in a layer
+layer.CreateFeature(feature1)
+layer.CreateFeature(feature2)
+print "The new extent"
+print layer.GetExtent()
+
+## So what is missing ????
+## Saving the file, but OGR doesn't have a Save() option
+## The shapefile is updated with all object structure 
+## when the script finished of when it is destroyed, 
+# if necessay SyncToDisk() maybe used
+
+ds.Destroy()
+## below the output is shown of the above Python script that is run in the terminal
+
+
+ 
+
 #file with symbol for point
-file_symbol=os.path.join("figs","arrow.png")
+file_symbol=os.path.join("data","marker2.png")
 
 #First we create a map
 map = mapnik.Map(800, 400) #This is the image final image size
@@ -65,7 +143,7 @@ map.layers.append(layerPoint)
 boundsLL = (1.3,51.979, 8.306,53.162  ) #(minx, miny, maxx,maxy)
 map.zoom_to_box(mapnik.Box2d(*boundsLL)) # zoom to bbox
 
-mapnik.render_to_file(map, os.path.join("figs",
-                                        "map3.png"), "png")
+mapnik.render_to_file(map, os.path.join("output",
+                                        "Mappie.png"), "png")
 print "All done - check content"
 
