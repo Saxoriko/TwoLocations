ccde87e050c674fe2d8941cadb1d282ecb5bd66a
user <william.schuch@wur.nl>
2016-01-18 15:49:35 +0100

    mymessage2

--- main.py
+++ main.py
@@ -1,3 +1,71 @@
 # main.py
 # Januari 18
 
+## Loading the modules
+import os
+import os,os.path
+import mapnik
+
+os.chdir('~/data')
+
+## Loading osgeo
+try:
+  from osgeo import ogr, osr
+  print 'Import of ogr and osr from osgeo worked.  Hurray!\n'
+except:
+  print 'Import of ogr and osr from osgeo failed\n\n'
+
+## Is the ESRI Shapefile driver available?
+driverName = "ESRI Shapefile"
+drv = ogr.GetDriverByName( driverName )
+if drv is None:
+    print "%s driver not available.\n" % driverName
+else:
+    print  "%s driver IS available.\n" % driverName
+
+#file with symbol for point
+file_symbol=os.path.join("figs","arrow.png")
+
+#First we create a map
+map = mapnik.Map(800, 400) #This is the image final image size
+
+#Lets put some sort of background color in the map
+map.background = mapnik.Color("steelblue") # steelblue == #4682B4 
+
+#Create the rule and style obj
+r = mapnik.Rule()
+s = mapnik.Style()
+
+polyStyle= mapnik.PolygonSymbolizer(mapnik.Color("darkred"))
+pointStyle = mapnik.PointSymbolizer(mapnik.PathExpression(file_symbol))
+r.symbols.append(polyStyle)
+r.symbols.append(pointStyle)
+
+s.rules.append(r)
+map.append_style("mapStyle", s)
+
+# Adding point layer
+layerPoint = mapnik.Layer("pointLayer")
+layerPoint.datasource = mapnik.Shapefile(file=os.path.join("data",
+                                        "twoLoopPoints.shp"))
+
+layerPoint.styles.append("mapStyle")
+
+#adding polygon
+layerPoly = mapnik.Layer("polyLayer")
+layerPoly.datasource = mapnik.Shapefile(file=os.path.join("data",
+                                        "ne_110m_land.shp"))
+layerPoly.styles.append("mapStyle")
+
+#Add layers to map
+map.layers.append(layerPoly)
+map.layers.append(layerPoint)
+
+#Set boundaries 
+boundsLL = (1.3,51.979, 8.306,53.162  ) #(minx, miny, maxx,maxy)
+map.zoom_to_box(mapnik.Box2d(*boundsLL)) # zoom to bbox
+
+mapnik.render_to_file(map, os.path.join("figs",
+                                        "map3.png"), "png")
+print "All done - check content"
+
